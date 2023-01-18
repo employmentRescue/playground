4860fcddc22e2351f7e8f185f6b852c188ef59e4
skylove0911 <skylove0911@naver.com>
2023-01-19 00:50:29 +0900

    FE-1000 feat react setup

--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+const container = document.getElementById('root') as HTMLElement;
+
+createRoot(container).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </StrictMode>
+);
--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,7 +1,19 @@
+import * as path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    !process.env.VITEST ?
+  checker({
+    typescript: true,
+  }) : undefined],
+  resolve:{
+    alias:{
+      '@': path.resolve(__dirname, './src'),
+    }
+  }
 })
