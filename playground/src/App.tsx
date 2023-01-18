e9669ea2a9e59adb21fbba62b3a16ab7084b765c
skylove0911 <skylove0911@naver.com>
2023-01-18 17:45:44 +0900

    FE-1000 feat install tailwindcss

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -24,7 +24,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-pink-600">
         Click on the Vite and React logos to learn more
       </p>
     </div>
