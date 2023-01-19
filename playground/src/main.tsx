9a3f22c6594043b62f3305279982603e6d4db9cc
skylove0911 <skylove0911@naver.com>
2023-01-19 16:45:06 +0900

    FE-1001 router구성 및 header footer layout 설계

--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -1,12 +1,27 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from '@/app/App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
+import DefaultPage from './pages/DefaultPage';
+import HomePage from './pages/HomePage';
 
 const container = document.getElementById('root') as HTMLElement;
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <DefaultPage />,
+    children: [
+      {
+        path: '',
+        element: <HomePage />,
+      }
+    ]
+  },
+]);
+
 
 createRoot(container).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
---
+++ playground/src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+export default function HomePage() {
+    return (
+        <div className="text-red-400">HELLO</div>
+    )
+}
