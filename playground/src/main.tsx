609242f7f283fd8b01b4313376625ef834990a48
ssg-js <likemath1997@gmail.com>
2023-01-19 10:41:35 +0900

    feat: playground

---
+++ playground/src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const container = document.getElementById('root') as HTMLElement;
+
+createRoot(container).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
