54f6f16b298b830c7adc9567bd832ac6089bdb7b
유도열 <oktui2002@naver.com>
2023-01-20 10:06:03 +0900

    add chat study

---
+++ YOODOYEOL/chat/src/main/java/com/ssafy/chat/WebSockConfig.java
@@ -0,0 +1,20 @@
+package com.ssafy.chat;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSocket
+public class WebSockConfig implements WebSocketConfigurer {
+    private final WebSocketHandler webSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");
+    }
+}
