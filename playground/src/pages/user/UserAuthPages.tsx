af3cbb21f58b1044e621a801bafa364edd7f7ffe
이강윤 <lky61010@naver.com>
2023-01-25 23:56:57 +0900

    FE-2000 chore 인증코드 추출 로직 작성

--- playground/src/pages/user/UserAuthPages.tsx
+++ playground/src/pages/user/UserAuthPages.tsx
@@ -1,4 +1,5 @@
 import { KAKAO_AUTH_URL } from "@/services/oauth"
+import { oauthRedirectHandler } from "@/services/oauthRedirectHandler"
 
 const LOGIN_URL = "http://localhost:3000/login"
 
@@ -9,14 +10,15 @@ export function LoginPage() {
             <h1 className="text-kakao">당신을 위한 스포츠 팀 매칭 서비스</h1>
             <a href={KAKAO_AUTH_URL}>
                 <img src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg"
-           width="242" />
+                    width="242" />
             </a>
         </div>
     )
 }
 
 export function LoginRegisterPage() {
-    return(
+    oauthRedirectHandler();
+    return (
         <div>
             <h1>카카오 로그인 성공!</h1>
             <h2>닉네임을 설정해주세요</h2>
@@ -25,10 +27,11 @@ export function LoginRegisterPage() {
 }
 
 export function LoginFailPage() {
-    return(
+    return (
         <div>
             <h1>로그인에 실패했어요ㅠㅠ</h1>
             <a href={LOGIN_URL}>다시 로그인하기</a>
         </div>
     )
 }
+
--- playground/src/services/oauthRedirectHandler.ts
+++ playground/src/services/oauthRedirectHandler.ts
@@ -1,3 +1,27 @@
-export function test() {
+import { useLocation } from "react-router-dom";
+import { CLIENT_ID, REDIRECT_URI } from "@/services/oauth"
+import { useEffect } from "react";
 
+export function oauthRedirectHandler() {
+    const location = useLocation();
+    const CODE_PARAM = location.search.split('=')[1];
+    console.log(CODE_PARAM)
+    const GRANT_TYPE = "authorization_code"
+    const KAKAO_AUTH_TOKEN_URL = `grant_type=${GRANT_TYPE}&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&code=${CODE_PARAM}`
+
+    const getKakaoToken = () => {
+        fetch(`https://kauth.kakao.com/oauth/token`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: KAKAO_AUTH_TOKEN_URL
+        })
+            .then(response => {
+                console.log(response)
+                response.json
+            })
+    }
+    useEffect(() => {
+        if (!location.search) return;
+        getKakaoToken();
+    }, [])
 }
