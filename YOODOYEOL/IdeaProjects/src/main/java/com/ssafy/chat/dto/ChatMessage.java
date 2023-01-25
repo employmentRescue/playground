7aa13902799afd4882ab99882f51fe07cfeab2be
유도열 <oktui2002@naver.com>
2023-01-25 18:01:55 +0900

    add project websocket

---
+++ YOODOYEOL/IdeaProjects/src/main/java/com/ssafy/chat/dto/ChatMessage.java
@@ -0,0 +1,19 @@
+package com.ssafy.chat.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessage {
+
+    public enum MessageType{
+        ENTER, TALK
+    }
+
+    private MessageType type;
+    private String roomId;
+    private String sender;
+    private String message;
+
+}
---
+++ YOODOYEOL/IdeaProjects/src/main/java/com/ssafy/chat/dto/ChatRoom.java
@@ -0,0 +1,37 @@
+package com.ssafy.chat.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+public class ChatRoom {
+
+    private String roomId;
+    private String name;
+    private Set<WebSocketSession> sessions = new HashSet<>();
+
+    @Builder
+    public ChatRoom(String roomId, String name){
+        this.roomId = roomId;
+        this.name = name;
+    }
+
+    public void handleActions(WebSocketSession session, ChatMessage chatMessage, ChatService chaetService){
+        if(chatMessage.getType().equals(ChatMessage.MessageType.ENTER)){
+            sessions.add(session);
+            chatMessage.setMessage(chatMessage.getSender() + "님이 입장했습니다.");
+        }
+        sendMessage(chatMessage, chatService);
+    }
+
+    public <T> void sendMessage(T message, ChatService chatService){
+        sessions.parallelStream().forEach(session -> chatService.sendMessage(session, message));
+    }
+
+}
