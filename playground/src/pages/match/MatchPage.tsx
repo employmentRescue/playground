3014e57c4f48c3783fb77d3580bca5b2f8b36199
ssg-js <likemath1997@gmail.com>
2023-01-26 13:40:22 +0900

    FE-3000 feat: contentbox 추가

--- playground/src/pages/match/MatchPage.tsx
+++ playground/src/pages/match/MatchPage.tsx
@@ -4,10 +4,18 @@ import { useReducer } from "react"
 
 type Tab = { type: 'AUTOMATCH' | 'LIST' };
 
+function ContentBox() {
+    return (
+        <div className="static top-16.75 w-41 h-169.5 mx-3 my-0 grow-0 bg-[#f5f5f5]">
+            <h1>hi</h1>
+        </div>
+    )
+}
+
 export default function MatchPage() {
     return (
         <div>
-            <h1>match page</h1>
+            <ContentBox></ContentBox>
         </div>
     )
 }
