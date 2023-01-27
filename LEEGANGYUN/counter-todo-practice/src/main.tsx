f2cdc1c2ca7bb3b453229dc3fd0b80267c2cec25
이강윤(Gangyun Lee) <109258306+GangYunGit@users.noreply.github.com>
2023-01-27 16:47:39 +0900

    Update counter-todo-practice: 새로고침해도 컴포넌트가 유지되도록 persistReducer, persistStore 설정 추가

--- LEEGANGYUN/counter-todo-practice/src/main.tsx
+++ LEEGANGYUN/counter-todo-practice/src/main.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import persistStore from 'redux-persist/es/persistStore';
 import Counter from './components/counter';
 import Home from './components/home';
 import Todo from './components/todo';
@@ -8,6 +10,7 @@ import DefaultPage from './DefaultPage';
 import './index.css'
 import store from "./store/store";
 
+let persistor = persistStore(store);
 
 const container = document.getElementById('root') as HTMLElement
 const router = createBrowserRouter([
@@ -34,6 +37,8 @@ const router = createBrowserRouter([
 
 createRoot(container).render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}/>
     <RouterProvider router={router} />
+
   </Provider>,
 )
--- LEEGANGYUN/counter-todo-practice/src/store/store.tsx
+++ LEEGANGYUN/counter-todo-practice/src/store/store.tsx
@@ -1,12 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { combineReducers,configureStore } from "@reduxjs/toolkit"
+import { persistReducer } from "redux-persist"
 import counterSlice from "./slices/counterSlice";
+import storage from "redux-persist/lib/storage";
 import todoSlice from "./slices/todoSlice";
 
+const reducers = combineReducers({
+  counter: counterSlice.reducer,
+  todo: todoSlice.reducer,
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 const store = configureStore({
-  reducer: {
-    counter: counterSlice.reducer,
-    todo: todoSlice.reducer
-  }
+  reducer: persistedReducer,
 });
 
 export default store;
