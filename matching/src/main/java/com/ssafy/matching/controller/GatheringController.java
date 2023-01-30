09900ef704ea7a2e09508925b40b35423fec1355
mwtgal20 <mwtgal20@gmail.com>
2023-01-30 23:28:14 +0900

    FEAT : findGatheringsByFilter 구현

--- matching/src/main/java/com/ssafy/matching/controller/GatheringController.java
+++ matching/src/main/java/com/ssafy/matching/controller/GatheringController.java
@@ -6,11 +6,11 @@ import com.ssafy.matching.service.GatheringService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -24,6 +24,7 @@ public class GatheringController {
 
     private GatheringService gatheringService;
 
+    @Autowired
     public GatheringController(GatheringService gatheringService) {
         this.gatheringService = gatheringService;
     }
