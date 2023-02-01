c1391afbdcd98900c50aac03de8ff220bce04a99
mwtgal20 <mwtgal20@gmail.com>
2023-02-01 23:50:09 +0900

    FIX : Gathering 칼럼 누락된거 추가(수정)

--- matching/src/main/java/com/ssafy/matching/dto/Gathering.java
+++ matching/src/main/java/com/ssafy/matching/dto/Gathering.java
@@ -36,7 +36,7 @@ public class Gathering implements Serializable {
     @ApiModelProperty(value = "운동 모임 시작 시간", required = true)
     private String startTime;
     @ApiModelProperty(value = "운동 모임 지속 시간", required = true)
-    private String playTime;
+    private int playTime;
     @ApiModelProperty(value = "운동 모임 모집 완료 여부", required = true)
     private boolean isCompleted;
     @ApiModelProperty(value = "모임장의  memberId", required = true)
