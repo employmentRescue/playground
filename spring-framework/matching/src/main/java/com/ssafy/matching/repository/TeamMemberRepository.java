eb0d4b9c09d41e35aa550af3d4e33c8df44618d8
mwtgal20 <mwtgal20@gmail.com>
2023-02-02 17:51:48 +0900

    BE-1007 FEAT : Team 관련 Repository 생성

---
+++ spring-framework/matching/src/main/java/com/ssafy/matching/repository/TeamMemberRepository.java
@@ -0,0 +1,9 @@
+package com.ssafy.matching.repository;
+
+import com.ssafy.matching.dto.TeamMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamMemberRepository extends JpaRepository<TeamMember, Integer> {
+    TeamMember save(TeamMember teamMember);
+    void deleteByTeamIdAndAndMemberId(int teamId, long memberId);
+}
