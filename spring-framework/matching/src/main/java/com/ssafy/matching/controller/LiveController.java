44060fa6bea0ce26ad301da5f11722b6f454d341
mwtgal20 <mwtgal20@gmail.com>
2023-02-02 15:55:49 +0900

    BE-1006 MODIFY : develop branch pull 후, 폴더 구조 변경

---
+++ spring-framework/matching/src/main/java/com/ssafy/matching/controller/LiveController.java
@@ -0,0 +1,91 @@
+package com.ssafy.matching.controller;
+
+import com.ssafy.matching.dto.Live;
+import com.ssafy.matching.dto.LiveMember;
+import com.ssafy.matching.service.LiveService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/live")
+@Api("실시간 운동 모임 정보 API")
+public class LiveController {
+    private static final String SUCCESS = "success";
+    private static final String FAIL = "fail";
+
+    private LiveService liveService;
+
+    @ApiOperation(value = "실시간 운동 모임 전체 리스트", notes = "내 위치 기준 반경 3km 안의 모든 실시간 운동 모임을 반환한다", response = List.class)
+    @GetMapping
+    public ResponseEntity<?> list(@RequestParam float lat, @RequestParam float lng) {
+        try {
+            List<Live> list = liveService.findLiveByLocation(lat, lng);
+
+            if (list != null && !list.isEmpty()) {
+                System.out.println(list);
+                return new ResponseEntity<List<Live>>(list, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+            }
+        } catch (Exception e) {
+            return exceptionHandling(e);
+        }
+
+    }
+
+    @ApiOperation(value = "실시간 운동 모임 상세 보기", notes = "실시간 운동 모임 id에 해당하는 실시간 운동 모임을 반환한다", response = Live.class)
+    @GetMapping("/{liveid}")
+    public ResponseEntity<?> view(@PathVariable("liveid") int liveId) throws Exception {
+        System.out.println("LiveController view() 실행");
+        return new ResponseEntity<Live>(liveService.getByLiveId(liveId), HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "실시간 운동 모임 등록하기", notes = "새로 실시간 운동 모임을 등록한다.")
+    @PostMapping("/register")
+    public void register(@RequestBody @ApiParam(value = "실시간 운동 모임 정보", required = true) Live live) throws Exception {
+        System.out.println(live);
+
+        liveService.registerLive(live);
+    }
+
+    @ApiOperation(value = "실시간 운동 모임 수정하기", notes = "실시간 운동 모임을 수정한다.")
+    @PutMapping
+    public void update(@RequestBody @ApiParam(value = "실시간 운동 모임 정보", required = true) Live live) throws Exception {
+        System.out.println(live);
+
+        liveService.updateLive(live);
+    }
+
+    @ApiOperation(value = "실시간 운동 모임 삭제하기", notes = "실시간 운동 모임 Id에 해당하는 실시간 운동 모임을 수정한다.")
+    @DeleteMapping("/{liveid}")
+    public void delete(@PathVariable("liveid") int liveId) throws Exception {
+        liveService.deleteLive(liveId);
+    }
+
+    @ApiOperation(value = "실시간 운동 모임에 참여하기", notes = "실시간 운동 모임에 참여한다.")
+    @PostMapping("/join")
+    public void join(@RequestBody @ApiParam(value = "실시간 운동 모임 멤버 정보", required = true) LiveMember liveMember) throws Exception {
+        System.out.println(liveMember);
+
+        liveService.joinLive(liveMember);
+    }
+
+    @ApiOperation(value = "실시간 운동 모임 참여 취소하기", notes = "모임Id와 유저ID에 해당하는 운동 모임을 삭제한다.")
+    @DeleteMapping("/leave/{gatheringid}")
+    public void delete(int liveId, long memberId) throws Exception {
+        liveService.leaveLive(liveId, memberId);
+    }
+
+    private ResponseEntity<?> exceptionHandling(Exception e) {
+        e.printStackTrace();
+        return new ResponseEntity<String>("Error : " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
---
+++ spring-framework/matching/src/main/java/com/ssafy/matching/service/GatheringServiceImpl.java
@@ -0,0 +1,100 @@
+package com.ssafy.matching.service;
+
+import com.ssafy.matching.dto.Gathering;
+import com.ssafy.matching.dto.GatheringMember;
+import com.ssafy.matching.repository.GatheringRepository;
+import com.ssafy.matching.repository.GatheringMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Transactional
+public class GatheringServiceImpl implements GatheringService {
+    @Autowired
+    private GatheringRepository gatheringRepository;
+    @Autowired
+    private GatheringMemberRepository gatheringMemberRepository;
+
+    @Override
+    public List<Gathering> findAll() {
+        System.out.println("findAll service 실행");
+        return gatheringRepository.findAllByIsCompletedFalse();
+    }
+
+    @Override
+    public List<Gathering> findGatheringsByFilter(Map<String, Object> map) {
+        //필터 조건 : 날짜, 지역(latX, latY), 반경, 최소 시작시간, 최대 시작시간, 수준, 최소 게임시간, 최대 게임시간, 성별, 게임종류
+        String startDate = (String)map.get("startDate");
+        float latX =  (float)map.get("latX");
+        float latY =  (float)map.get("latX");
+        int distance = (int)map.get("distance");
+        int minStartTime = (int)map.get("minStartTime");
+        int maxStartTime = (int)map.get("maxStartTime");
+        String level = (String)map.get("level");
+        int minPlayTime = (int)map.get("minPlayTime");
+        int maxPlayTime = (int)map.get("maxPlayTime");
+        String sex = (String)map.get("sex");
+        String sports = (String)map.get("sports");
+        String gameType = (String)map.get("gameType");
+        
+        //TODO sort 구현해야 함
+        return gatheringRepository.findGatheringsByFilter(startDate, latX, latY, distance, minStartTime, maxStartTime, level, minPlayTime, maxPlayTime, sex, sports, gameType);
+    }
+
+    @Override
+    public Gathering getByGatheringId(int gatheringId) {
+        System.out.println("getByGatheringId service 실행");
+        return gatheringRepository.getByGatheringId(gatheringId);
+    }
+
+    @Override
+    public void registerGathering(Gathering gathering) {
+        Gathering savedGathering =  gatheringRepository.save(gathering);
+        gatheringMemberRepository.save(new GatheringMember(0, savedGathering.getGatheringId(), savedGathering.getHostId())); //호스트 멤버에 넣기
+    }
+
+    @Override
+    public void updateGathering(Gathering gathering) {
+        gatheringRepository.save(gathering);
+    }
+
+    @Override
+    public void deleteGathering(int gatheringId) {
+        gatheringMemberRepository.deleteByGatheringId(gatheringId);
+        gatheringRepository.deleteByGatheringId(gatheringId);
+    }
+
+    @Override
+    public void joinGathering(GatheringMember memberGathering) {
+        gatheringMemberRepository.save(memberGathering);
+        
+        //TODO 의문점
+        int gatheringId = memberGathering.getGatheringId();
+        Gathering gathering = gatheringRepository.getByGatheringId(gatheringId);
+
+        System.out.println("size : " + gathering.getMemberGatheringList().size()); //왜 이 시점에서는 앞에 결과가 반영이 안될까...?
+
+        if(gathering.getMemberGatheringList().size() + 1 == gathering.getPeople()) { //위에 문제 때문에 +1 해줌..
+            gathering.setCompleted(true);
+            System.out.println("gathering : " + gathering);
+            gatheringRepository.save(gathering);
+        }
+    }
+
+    @Override
+    public void leaveGathering(int gatheringId, long memberId) {
+        //나가려는 멤버가 호스트이면, 호스트 다른 사람에게 넘겨주고 떠나기
+        Gathering gathering = gatheringRepository.getByGatheringId(gatheringId);
+
+        if(memberId == gathering.getHostId()) {
+            long nextHostId = gathering.getMemberGatheringList().get(1).getMemberId();
+            gathering.setHostId(nextHostId);
+        }
+
+        gatheringMemberRepository.deleteByGatheringIdAndMemberId(gatheringId, memberId);
+    }
+}
