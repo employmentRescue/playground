19c884c3a856f1c50d842e9908b173baeed51634
hoan123432 <hoan123432@naver.com>
2023-02-04 00:11:08 +0900

    Merge branch 'feature-BE-4001(user-service)' of https://lab.ssafy.com/s08-webmobile2-sub2/S08P12B309 into develop

--- spring-framework/oauth-service/src/main/java/com/ssafy/oauth_service/Repository/KakaoLoginRefreshTokenCacheRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.oauth_service.Repository;
-
-import com.ssafy.oauth_service.dto.KakaoLoginRefreshTokenCache;
-import org.springframework.data.repository.CrudRepository;
-
-public interface KakaoLoginRefreshTokenCacheRepository extends CrudRepository<KakaoLoginRefreshTokenCache, String> {
-}
