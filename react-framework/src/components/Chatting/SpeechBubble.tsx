7742c220dfb9f8698a68655dc3b8834dd7c089a1
이강윤(Gangyun Lee) <109258306+GangYunGit@users.noreply.github.com>
2023-02-03 10:25:10 +0900

    FE-2007 style: 나의 말풍선 전용 색 추가

--- react-framework/src/components/Chatting/SpeechBubble.tsx
+++ react-framework/src/components/Chatting/SpeechBubble.tsx
@@ -8,17 +8,24 @@ interface SpeechBubbleProps {
 
 export function SpeechBubble({ innerText, isMine, profile }: SpeechBubbleProps) {
     let bubbleReverse = ""
+    let squareBgColor = ""
+    let triangleBgColor = ""
     if (isMine) {
-        bubbleReverse = "-scale-x-100"
+        bubbleReverse = "-scale-x-100 "
+        squareBgColor = "bg-blue-100 "
+        triangleBgColor = "border-b-blue-100 "
+    } else {
+        squareBgColor = "bg-white "
+        triangleBgColor = "border-b-white "
     }
     return (
         <div className={"flex " + bubbleReverse}>
             <Profile profile={profile}/>
-            <div className="border-b-12 border-b-white border-l-transparent border-l-0 border-r-16 border-r-transparent border-solid rotate-180 mt-20 ml-5">
+            <div className={"border-b-12 border-l-transparent border-l-0 border-r-16 border-r-transparent border-solid rotate-180 mt-20 ml-5 " + triangleBgColor}>
 
             </div>
             <div className="flex w-[200px] my-10">
-                <div className={"w-auto h-auto bg-white rounded-10 p-12 font-inter text-15 justify-self-start " + bubbleReverse}>
+                <div className={"w-auto h-auto rounded-10 p-12 font-inter text-15 justify-self-start " + bubbleReverse + squareBgColor}>
                     <p>{innerText}</p>
                 </div>
             </div>
