76b123a299bca9518ffb1bd7c77f99003528ef39
이강윤 <lky61010@naver.com>
2023-02-04 22:23:04 +0900

    FE-2008 feat: 말풍선 컴포넌트 닉네임, 시간 구현

--- react-framework/src/components/Chatting/SpeechBubble.tsx
+++ react-framework/src/components/Chatting/SpeechBubble.tsx
@@ -1,17 +1,25 @@
 import { Profile } from "./Profile"
+import dayjs from "dayjs"
 
 interface SpeechBubbleProps {
     isMine: boolean
     innerText: string
     profile: string
+    nickName?: string
+    dateTime?: Date
 }
 
-export function SpeechBubble({ innerText, isMine, profile }: SpeechBubbleProps) {
-    let bubbleReverse = ""
+export function SpeechBubble({ innerText, isMine, profile, nickName, dateTime }: SpeechBubbleProps) {
+    let Reversed = ""
     let squareBgColor = ""
     let triangleBgColor = ""
+
+    function dateTimeFormat(dateTime: Date | undefined) {
+        if (dateTime) return dayjs(dateTime).format("hh:mm A")
+    }
+
     if (isMine) {
-        bubbleReverse = "-scale-x-100 "
+        Reversed = "-scale-x-100 "
         squareBgColor = "bg-blue-100 "
         triangleBgColor = "border-b-blue-100 "
     } else {
@@ -19,14 +27,22 @@ export function SpeechBubble({ innerText, isMine, profile }: SpeechBubbleProps) 
         triangleBgColor = "border-b-white "
     }
     return (
-        <div className={"flex bg-gray-100 " + bubbleReverse}>
-            <Profile profile={profile} className="mt-5 ml-10" />
-            <div className={"border-b-12 border-l-transparent border-l-0 border-r-16 border-r-transparent border-solid rotate-180 mt-20 ml-5 " + triangleBgColor}>
-
+        <div className={"flex bg-gray-100 -m-5 " + Reversed}>
+            <Profile profile={profile} className={"mt-10 ml-15 " + Reversed} />
+            <div className={"border-b-12 border-l-transparent border-l-0 border-r-16 border-r-transparent border-solid rotate-180 mt-30 ml-5 " + triangleBgColor}>
             </div>
-            <div className="flex w-[200px] my-10">
-                <div className={"w-auto h-auto -ml-2 rounded-10 p-12 font-inter text-15 tracking-tight justify-self-start " + bubbleReverse + squareBgColor}>
-                    <p>{innerText}</p>
+
+            <div className="flex flex-col w-auto my-10 mr-50">
+                <div className="flex flex-col">
+                    <div className={"-mt-5 font-inter text-10 tracking-tight " + (isMine && "pt-15 ")}>{nickName}</div>
+                    <div className={"flex "}>
+                        <div className={"h-auto -ml-2 rounded-10 p-12 font-inter text-15 tracking-tight justify-self-start " + squareBgColor + Reversed}>
+                            <p>{innerText}</p>
+                        </div>
+                        <div className={Reversed + "ml-4 self-end text-10"}>
+                            <p className="w-48 text-[#969696]">{dateTimeFormat(dateTime)}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- react-framework/src/pages/chatting/ChattingRoomPage.tsx
+++ react-framework/src/pages/chatting/ChattingRoomPage.tsx
@@ -3,69 +3,94 @@ import { SpeechBubble } from "@/components/Chatting/SpeechBubble";
 import defaultProfile from "@/assets/profiles/default-profile.png"
 import emoticonButton from "@/assets/icons/chatting-emoticon.png"
 import sendButton from "@/assets/icons/send-message-button.png"
-import { useState, useRef, useCallback, useEffect } from "react"
+import { useState, useRef, useEffect } from "react"
 
-type TextList = { isMine: boolean, innerText: string, profile: string }[]
-
+type TextList = {
+    isMine: boolean,
+    nickName?: string,
+    innerText: string,
+    profile: string,
+    dateTime?: Date,
+}[]
 
 export default function ChattingRoomPage() {
     const params = useParams();
     const initialTextList: TextList = [
         {
             isMine: false,
+            nickName: "닉네임1",
             innerText: "안녕하세요. 반가워요",
-            profile: defaultProfile
+            profile: defaultProfile,
+            dateTime: new Date("2023-02-04 21:11:04")
         },
         {
             isMine: false,
+            nickName: "축구1",
             innerText: "축구할 사람?",
-            profile: defaultProfile
+            profile: defaultProfile,
+            dateTime: new Date("2023-02-04 21:11:04")
         },
         {
             isMine: true,
             innerText: "오늘 야근이에요ㅠㅠ",
-            profile: defaultProfile
+            profile: defaultProfile,
+            dateTime: new Date("2023-02-04 21:11:04")
         },
         {
             isMine: false,
+            nickName: "축구1",
             innerText: "아쉽네요..",
-            profile: defaultProfile
+            profile: defaultProfile,
+            dateTime: new Date("2023-02-04 21:11:04")
         },
         {
             isMine: true,
             innerText: "내일 6시에 축구 하실분 계신가요?",
-            profile: defaultProfile
+            profile: defaultProfile,
+            dateTime: new Date("2023-02-04 21:11:04")
         },
         {
             isMine: false,
+            nickName: "축구2",
             innerText: "축구 ㄱ?",
-            profile: defaultProfile
+            profile: defaultProfile,
+            dateTime: new Date("2023-02-04 21:11:04")
         }
     ]
     const [textList, setTextList] = useState(initialTextList)
     const [activateSend, setActivateSend] = useState("opacity-40")
     const [inputValue, setInputValue] = useState("")
-
     const newTextList = {
         isMine: true,
         innerText: inputValue,
-        profile: defaultProfile
+        profile: defaultProfile,
+        dateTime: new Date()
     }
     let scrollRef: any | undefined = useRef(null);
     let inputRef: any | undefined = useRef(null);
 
+    // 서버에서 불러온 해당 채팅방의 모든 채팅을 화면에 렌더링 해줄 함수
     const TextListRendering = () => {
         let index = 0
         const Result = textList.map((text) => {
             index++;
-            return <SpeechBubble key={index} isMine={text.isMine} innerText={text.innerText} profile={text.profile} />
+            return (
+                <SpeechBubble
+                    key={index}
+                    isMine={text.isMine}
+                    nickName={text.nickName}
+                    innerText={text.innerText}
+                    profile={text.profile}
+                    dateTime={text.dateTime}
+                />
+            )
+
         })
         return Result
     }
 
-
+    // 메시지 입력창의 텍스트를 얻어오는 함수
     const handleOnChange = (e: any) => {
-        console.log(e.target.value)
         setInputValue(e.target.value)
         if (e.target.value) {
             setActivateSend("")
@@ -74,11 +99,10 @@ export default function ChattingRoomPage() {
         }
     }
 
+    // Enter 입력시 메시지 입력창에 입력된 텍스트를 전송
     const handleKeyPress = (e: any) => {
         if (e.code === "Enter") {
             if (!inputValue) return
-            console.log(e.target.value)
-
             setTextList(
                 [...textList, newTextList]
             )
@@ -88,6 +112,7 @@ export default function ChattingRoomPage() {
         }
     }
 
+    // 버튼 클릭으로도 텍스트 전송이 가능
     function handleOnClick() {
         if (!inputValue) return
         setTextList([...textList, newTextList])
@@ -103,7 +128,7 @@ export default function ChattingRoomPage() {
     return (
         <div className="w-full h-full flex flex-col justify-between bg-gray-100">
             <div>
-                <div className="flex justify-center font-inter text-20">
+                <div className="flex justify-center font-inter text-20 my-10">
                     {params.roomId}번 채팅방
                 </div>
                 <div ref={scrollRef}>{TextListRendering()}</div>
@@ -114,7 +139,7 @@ export default function ChattingRoomPage() {
                 <input
                     value={inputValue}
                     onChange={(e) => handleOnChange(e)}
-                    className="ml-13 w-full sticky bottom-55"
+                    className="ml-13 w-full sticky bottom-55 outline-none"
                     placeholder="메시지 입력"
                     onKeyPress={(e) => handleKeyPress(e)}
                     ref={inputRef}
