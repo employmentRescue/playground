491753166c69be0f085493ffc67ea211e6416004
mwtgal20 <mwtgal20@gmail.com>
2023-02-07 00:24:51 +0900

    BE-1011 FIX : Team - update 수정

--- spring-framework/matching/src/main/java/com/ssafy/matching/controller/TeamController.java
+++ spring-framework/matching/src/main/java/com/ssafy/matching/controller/TeamController.java
@@ -37,7 +37,7 @@ public class TeamController {
     public ResponseEntity<Team> update(@RequestBody @ApiParam(value = "팀 정보", required = true) Team team) throws Exception {
         System.out.println(team);
 
-        return new ResponseEntity<Team>(teamService.registerTeam(team), HttpStatus.OK);
+        return new ResponseEntity<Team>(teamService.updateTeam(team), HttpStatus.OK);
     }
 
     @ApiOperation(value = "팀 삭제하기(구현중)", notes = "팀Id에 해당하는 팀을 삭제한다.")
