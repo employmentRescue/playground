6bf8e879082572cb07f7582149bd3e2caa1ae8ea
이강윤(Gangyun Lee) <109258306+GangYunGit@users.noreply.github.com>
2023-02-11 18:30:18 +0900

    FE-2015 feat: 프로필 수정 페이지 선호 지역선택 추가

--- react-framework/src/components/Chatting/ChatListModal.tsx
+++ react-framework/src/components/Chatting/ChatListModal.tsx
@@ -33,17 +33,17 @@ export default function ChatListModal({ showModal, setShowModal }: Iprops) {
 
   return (
     <div className="flex flex-col z-20" >
-      <div className="flex justify-center w-[90%] h-[120px] self-center bg-white rounded-15 fixed right-[50%] translate-x-[50%] bottom-[10%] z-10">
+      <div className="flex justify-center w-[90%] h-[120px] self-center bg-white rounded-15 fixed right-[50%] translate-x-[50%] bottom-[10%] z-10" >
         <div className="flex flex-col w-[90%]">
-          <div className="mt-15 mb-5 w-46 h-3 text-16 self-center font-inter font-bold bg-[#DBDBDB]" />
-          <div className="flex p-10">
-            <img src={trashIcon} className="flex w-13 h-15 self-center" />
-            <p className="ml-15 mr-60 font-bold text-15 font-inter tracking-tight" onClick={handleOnClickExit}>나가기</p>
+          <div className="mt-15 mb-15 mx-30 w-46 h-3 text-16 self-center font-inter font-bold bg-[#DBDBDB]" onClick={() => setShowModal(!showModal)} />
+          <div className="flex p-10 -mt-10">
+            <img src={trashIcon} className="flex w-13 h-15 mt-2" />
+            <p className="flex ml-15 mr-60 self-center font-bold text-15 font-inter tracking-tight" onClick={handleOnClickExit}>나가기</p>
           </div>
           <div className="flex p-10 justify-between">
             <div className="flex">
-              <img src={notificationIcon} className="flex w-13 h-15 self-center" />
-              <p className="ml-15 font-bold text-15 font-inter tracking-tight">알림</p>
+              <img src={notificationIcon} className="flex w-13 h-15 mt-2" />
+              <p className="flex ml-15 font-bold text-15 font-inter tracking-tight">알림</p>
             </div>
             <div className="flex">
               <NotificationToggleButton notiTogggle={notiTogggle} setNotiTogggle={setNotiTogggle} />
