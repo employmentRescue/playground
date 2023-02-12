454888797753662150650859f5d297f94cb9e03b
김태훈 <hoan123432@naver.com>
2023-02-12 15:08:15 +0900

    Deleted spring-framework/idclass-test-2/src/main/java/com/example/idclasstest2/activitiesEntity.java, spring-framework/idclass-test-2/src/main/java/com/example/idclasstest2/activitiesId.java, spring-framework/idclass-test-2/src/main/java/com/example/idclasstest2/activityRepository.java, spring-framework/idclass-test-2/src/main/resources/application.yml, spring-framework/idclass-test-2/src/main/resources/log4j2.xml

--- spring-framework/idclass-test-2/src/main/java/com/example/idclasstest2/activityRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.idclasstest2;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface activityRepository extends JpaRepository<activitiesEntity, activitiesId> {
-}
