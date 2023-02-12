d0ee084b10b82354567678371684986f7f31b844
김태훈 <hoan123432@naver.com>
2023-02-12 16:13:06 +0900

    Deleted spring-framework/oauth_service/gradle/wrapper/gradle-wrapper.jar, spring-framework/oauth_service/gradle/wrapper/gradle-wrapper.properties, spring-framework/oauth_service/src/main/java/com/ssafy/oauth_service/Repository/KakaoLoginAccessTokenCacheRepository.java, spring-framework/oauth_service/src/main/java/com/ssafy/oauth_service/Repository/KakaoLoginRefreshTokenCacheRepository.java, spring-framework/oauth_service/src/main/java/com/ssafy/oauth_service/Repository/OAuthRegisterCacheRepository.java, spring-framework/oauth_service/src/main/java/com/ssafy/oauth_service/config/CorsFilter.java, spring-framework/oauth_service/src/main/java/com/ssafy/oauth_service/config/JacksonConfig.java, spring-framework/oauth_service/src/main/java/com/ssafy/oauth_service/config/QueryDslConfig.java, spring-framework/oauth_service/src/main/java/com/ssafy/oauth_service/config/RedisConfig.java, spring-framework/oauth_service/src/main/java/com/ssafy/oauth_service/config/WebConfig.java, spring-framework/oauth_service/src/main/java/com/ssafy/oauth_service/config/cross_originConfig.java, spring-framework/oauth_service/src/main/java/com/ssafy/oauth_service/controller/UserInfoController.java, spring-framework/oauth_service/src/main/java/com/ssafy/oauth_service/controller/fileController.java, spring-framework/oauth_service/src/main/java/com/ssafy/oauth_service/controller/testController.java, spring-framework/oauth_service/src/main/java/com/ssafy/oauth_service/dto/BaseTimeEntity.java, spring-framework/oauth_service/src/main/java/com/ssafy/oauth_service/dto/FileTableType.java, spring-framework/oauth_service/src/main/java/com/ssafy/oauth_service/dto/MemberOftenEntity.java, spring-framework/oauth_service/src/main/java/com/ssafy/oauth_service/dto/MemberSometimesEntity.java, spring-framework/oauth_service/src/main/java/com/ssafy/oauth_service/dto/activitiesEntity.java, spring-framework/oauth_service/src/main/java/com/ssafy/oauth_service/dto/fileEntity.java, spring-framework/oauth_service/src/main/java/com/ssafy/oauth_service/OauthServiceApplication.java, spring-framework/oauth_service/src/main/java/com/ssafy/oauth_service/UserServiceApplication.java, spring-framework/oauth_service/src/main/resources/application.properties, spring-framework/oauth_service/src/main/resources/application.yml, spring-framework/oauth_service/src/main/resources/keystore.p12, spring-framework/oauth_service/src/test/java/com/ssafy/oauth_service/OauthServiceApplicationTests.java, spring-framework/oauth_service/.gitignore, spring-framework/oauth_service/Dockerfile, spring-framework/oauth_service/build.gradle, spring-framework/oauth_service/gradlew, spring-framework/oauth_service/gradlew.bat, spring-framework/oauth_service/settings.gradle, spring-framework/user-service/build/classes/java/main/com/ssafy/userservice/dto/QMemberOftenEntity.class, spring-framework/user-service/build/classes/java/main/com/ssafy/userservice/dto/QMemberSometimesEntity.class

--- spring-framework/oauth_service/src/main/java/com/ssafy/oauth_service/Repository/OAuthRegisterCacheRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.oauth_service.Repository;
-
-import com.ssafy.oauth_service.dto.OAuthRegisterCache;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OAuthRegisterCacheRepository extends CrudRepository<OAuthRegisterCache, String> {
-
-}
--- spring-framework/oauth_service/src/main/java/com/ssafy/oauth_service/dto/FileTableType.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.userservice.dto;
-
-public enum FileTableType {
-    user_profile_img("user"), match_img("match");
-
-    private final String value;
-    FileTableType(String value) { this.value = value; }
-    public String getValue(){ return value; }
-}
