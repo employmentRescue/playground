e18b408b75fb5b978cd056f068d457f19a31bf23
hoan123432 <hoan123432@naver.com>
2023-02-14 14:00:40 +0900

    user-service cors1

--- spring-framework/user-service/src/main/java/com/ssafy/userservice/config/CorsFilter.java
+++ spring-framework/user-service/src/main/java/com/ssafy/userservice/config/CorsFilter.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
@@ -20,37 +19,36 @@ public class CorsFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
 
-
-
-
-//        System.out.println("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
-//
-       String url = ((HttpServletRequest)req).getRequestURL().toString();
-       String queryString = ((HttpServletRequest)req).getQueryString();
-       System.out.println(((HttpServletRequest)req).getRequestURL().toString());
-//
-//
+        // System.out.println("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+        //
+        String url = ((HttpServletRequest) req).getRequestURL().toString();
+        String queryString = ((HttpServletRequest) req).getQueryString();
+        System.out.println(((HttpServletRequest) req).getRequestURL().toString());
+        //
+        //
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-//        response.setHeader("Access-Control-Allow-Origin", "*");
+        // response.setHeader("Access-Control-Allow-Origin", "*");
 
-//
-////        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-//        response.setHeader("Access-Control-Allow-Origin", "https://192.168.31.246/oauth2/login/kakao");
-//        response.setHeader("Access-Control-Allow-Origins", "*");
-//        response.setHeader("Access-Control-Allow-Methods","*");
-//        response.setHeader("Access-Control-Max-Age", "3600");
-//        response.setHeader("Access-Control-Allow-Headers",
-//                "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//
-//        if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-//            response.setStatus(HttpServletResponse.SC_OK);
-//        }else {
-//        }
-            chain.doFilter(req, res);
+        //
+        // response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        // response.setHeader("Access-Control-Allow-Origin",
+        // "https://192.168.31.246/oauth2/login/kakao");
+        // response.setHeader("Access-Control-Allow-Origins", "*");
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers",
+                "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        //
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+        }
+        chain.doFilter(req, res);
     }
 
     @Override
