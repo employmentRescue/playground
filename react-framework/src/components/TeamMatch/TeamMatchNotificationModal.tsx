7abb53066fe07cf170d2f480e6b0c0a18e7d49c0
skylove0911 <skylove0911@naver.com>
2023-02-15 18:00:14 +0900

    FE-1039 feat: firebase.js

--- react-framework/src/components/TeamMatch/TeamMatchNotificationModal.tsx
+++ react-framework/src/components/TeamMatch/TeamMatchNotificationModal.tsx
@@ -1,25 +1,38 @@
 import exitIcon from '@/assets/icons/exit.png'
+import { useEffect, useState } from 'react';
+import { requestForToken, onMessageListener } from '@/utils/firebase';
 
 export default function TeamMatchNotificationModal() {
-  return (
-    <div className="absolute w-full h-full bottom-0 bg-black/50 z-20">
-      <div className="w-[304px] h-[389px] absolute rounded-15 top-1/2 left-1/2 ml-[-152px] mt-[-194px] overflow-hidden">
-        <img className="w-10 h-10 absolute top-16 right-16" src={exitIcon}></img>
-        <div className="w-full h-134 flex flex-col justify-center items-center bg-green-300">
-          <img className="w-40 h-40"></img>
-          <div className="text-15 mt-18">매칭이 완료되었습니다</div>
-        </div>
-        <div className="w-full h-[255px] bg-white">
-          <div className="w-full h-196 flex flex-col items-center">
-            <img className="w-100 h-100 mt-19"></img>
-            <div className="text-15 mt-18">MUNKS</div>
-            <div className="w-88 h-30 mt-5 flex justify-center items-center bg-yellow-200 rounded-10">GOLD 2</div>
+  const [notification, setNotification] = useState({ title: '', body: '' });
+
+  useEffect(() => {
+    notify()
+  }, [notification])
+
+  requestForToken();
+
+  function notify() {
+    return (
+      <div className="absolute w-full h-full bottom-0 bg-black/50 z-20">
+        <div className="w-[304px] h-[389px] absolute rounded-15 top-1/2 left-1/2 ml-[-152px] mt-[-194px] overflow-hidden">
+          <img className="w-10 h-10 absolute top-16 right-16" src={exitIcon}></img>
+          <div className="w-full h-134 flex flex-col justify-center items-center bg-green-300">
+            <img className="w-40 h-40"></img>
+            <div className="text-15 mt-18">매칭이 완료되었습니다</div>
           </div>
-          <div className="w-full h-59">
-            <div className="absolute bottom-17 right-13 text-16 text-blue-700">자세히 보기</div>
+          <div className="w-full h-[255px] bg-white">
+            <div className="w-full h-196 flex flex-col items-center">
+              <img className="w-100 h-100 mt-19"></img>
+              <div className="text-15 mt-18">{notification.title}</div>
+              <div className="w-88 h-30 mt-5 flex justify-center items-center bg-yellow-200 rounded-10">{notification.body}</div>
+            </div>
+            <div className="w-full h-59">
+              <div className="absolute bottom-17 right-13 text-16 text-blue-700">자세히 보기</div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
+
 }
