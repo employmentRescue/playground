557af499b58177e8d961b9cb79cbbb1daf3555c6
이강윤(Gangyun Lee) <109258306+GangYunGit@users.noreply.github.com>
2023-02-15 09:13:32 +0900

    FE-2017 chore: 소켓 구현 중

--- react-framework/src/pages/chatting/ChattingListPage.tsx
+++ react-framework/src/pages/chatting/ChattingListPage.tsx
@@ -6,7 +6,7 @@ import autoMatchfootball from "@/assets/profiles/auto-match-football.png"
 import autoMatchBasketball from "@/assets/profiles/auto-match-basketball.png"
 import autoMatchBadminton from "@/assets/profiles/auto-match-badminton.png"
 
-import useGetAllChattingRoomsByMemberId from "@/hooks/chat/useGetAllChattingRoomsByMemberId"
+import useGetAllTeamChattingRoomsByMemberId from "@/hooks/chat/useGetAllChattingRoomsByMemberId"
 
 import { useDispatch, useSelector } from "react-redux"
 import { useEffect } from "react"
@@ -26,20 +26,24 @@ export default function ChattingListPage() {
         return state.userId
     })
     const dispatch = useDispatch();
-    const ChattingList = useGetAllChattingRoomsByMemberId(myUserId);
+    const { data } = useGetAllTeamChattingRoomsByMemberId(myUserId);
+
 
     useEffect(() => {
         dispatch(setTabName('채팅 목록'))
-        console.log(ChattingList)
+        data && console.log(data)
     }, [])
+
+    const renderChatRooms = () => {
+        return data && data.map((chattingRoom: any) => {
+            return <ListCard key={chattingRoom.teamChatroomId} roomId={chattingRoom.teamChatroomId} roomProfile={basketball} title={chattingRoom.chatroomName} personnel={chattingRoom.gameType} latestMsg="오늘 저녁 농구 ㄱ?" />
+        })
+    }
 
     return (
         <div className="flex flex-col h-auto">
             <Notice title="오늘 운동은 어떠셨나요?" content="팀원들에게 격려의 메세지를 남겨주세요!" />
-            <ListCard roomId={1} roomProfile={basketball} title="슬램덩크" personnel={6} latestMsg="오늘 저녁 농구 ㄱ?" />
-            <ListCard roomId={2} roomProfile={basketball2} title="대전농구팀" personnel={8} latestMsg="오늘 슬램덩크 팀과 대결 있습니다." />
-            <ListCard roomId={3} roomProfile={autoMatchfootball} title="01-16축구" personnel={22} latestMsg={"매칭이 완료되었습니다!"} />
-            <ListCard roomId={4} roomProfile={autoMatchBadminton} title="01-17배드민턴" personnel={2} latestMsg="매칭이 완료되었습니다!" />
+            {renderChatRooms()}
         </div>
     )
 }
--- react-framework/src/pages/chatting/ChattingRoomPage.tsx
+++ react-framework/src/pages/chatting/ChattingRoomPage.tsx
@@ -5,11 +5,16 @@ import sendButton from "@/assets/icons/send-message-button.png"
 import { setTabName } from "@/stores/tab/tabName";
 import { SpeechBubble } from "@/components/Chatting/SpeechBubble";
 import useWebSocket from "@/hooks/chat/useWebSocket";
+import useGetExactChattingRoom from "@/hooks/chat/useGetExactChattingRoomId";
 
 import { useParams } from "react-router-dom"
 import { useState, useRef, useEffect } from "react"
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
+import axios from "axios";
+import { RootState } from "@/stores/store";
+import SockJS from "sockjs-client";
+import { Stomp } from "@stomp/stompjs";
 
 type TextList = {
     roomId: number,
@@ -35,14 +40,6 @@ export default function ChattingRoomPage() {
         },
         {
             roomId: 1,
-            isMine: false,
-            nickName: "축구2",
-            innerText: "축구할 사람?",
-            profile: defaultProfile,
-            dateTime: new Date("2023-02-04 21:11:04")
-        },
-        {
-            roomId: 1,
             isMine: true,
             innerText: "오늘 야근이에요ㅠㅠ",
             profile: defaultProfile,
@@ -53,21 +50,6 @@ export default function ChattingRoomPage() {
             isMine: false,
             nickName: "축구3",
             innerText: "아쉽네요..",
-            profile: defaultProfile,
-            dateTime: new Date("2023-02-04 21:11:04")
-        },
-        {
-            roomId: 1,
-            isMine: true,
-            innerText: "내일 6시에 축구 하실분 계신가요?",
-            profile: defaultProfile,
-            dateTime: new Date("2023-02-04 21:11:04")
-        },
-        {
-            roomId: 1,
-            isMine: false,
-            nickName: "축구1",
-            innerText: "축구 ㄱ?",
             profile: defaultProfile,
             dateTime: new Date("2023-02-04 21:11:04")
         },
@@ -89,36 +71,6 @@ export default function ChattingRoomPage() {
             profile: defaultProfile,
             dateTime: new Date("2023-02-04 21:11:04")
         },
-        {
-            roomId: 2,
-            isMine: true,
-            innerText: "오늘 본가 내려가는 날이에요 ㅠㅠ",
-            profile: defaultProfile,
-            dateTime: new Date("2023-02-04 21:11:04")
-        },
-        {
-            roomId: 2,
-            isMine: false,
-            nickName: "농구1",
-            innerText: "아쉽네요..",
-            profile: defaultProfile,
-            dateTime: new Date("2023-02-04 21:11:04")
-        },
-        {
-            roomId: 2,
-            isMine: true,
-            innerText: "내일 6시에 농구 하실분 계신가요?",
-            profile: defaultProfile,
-            dateTime: new Date("2023-02-04 21:11:04")
-        },
-        {
-            roomId: 2,
-            isMine: false,
-            nickName: "농구2",
-            innerText: "농구 ㄱ?",
-            profile: defaultProfile,
-            dateTime: new Date("2023-02-04 21:11:04")
-        },
 
 
         {
@@ -126,29 +78,6 @@ export default function ChattingRoomPage() {
             isMine: false,
             nickName: "배민1",
             innerText: "안녕하세요. 반가워요",
-            profile: defaultProfile,
-            dateTime: new Date("2023-02-04 21:11:04")
-        },
-        {
-            roomId: 3,
-            isMine: false,
-            nickName: "배민2",
-            innerText: "배드민턴할 사람?",
-            profile: defaultProfile,
-            dateTime: new Date("2023-02-04 21:11:04")
-        },
-        {
-            roomId: 3,
-            isMine: true,
-            innerText: "제가 오늘 몸이 안좋아서요ㅠㅠ",
-            profile: defaultProfile,
-            dateTime: new Date("2023-02-04 21:11:04")
-        },
-        {
-            roomId: 3,
-            isMine: false,
-            nickName: "배민2",
-            innerText: "아쉽네요..",
             profile: defaultProfile,
             dateTime: new Date("2023-02-04 21:11:04")
         },
@@ -195,8 +124,38 @@ export default function ChattingRoomPage() {
         profile: defaultProfile,
         dateTime: new Date()
     }
+    const myUserId = useSelector((state: RootState) => {
+        return state.userId
+    })
     let scrollRef: any | undefined = useRef(null);
     let inputRef: any | undefined = useRef(null);
+
+
+    const sock = new SockJS("/ws-stomp")
+    const webSocket = Stomp.over(sock);
+    // const stompClient = useRef<CompatClient>()
+
+    // const recvMessage = (message: any) => {
+    //     textList.push({ "chatroomId": message.chatroomId, "regTime": "10", "memberId": message.type == 'ENTER' ? '[알림]' : message.memberId, "content": message.content, "isNotice": false, "type": message.type })
+    //     axios.post('/chat/readMessage/' + params.roomId + '?memberId=' + myUserId, message);
+    // }
+
+    const sendMessage = (content: string) => {
+        webSocket.send("/pub/chat/Message", {}, JSON.stringify({ chatroomId: params.roomId, regTime: '10', memberId: myUserId, content: content, isNotice: false, type: 'TALK' }));
+        content = '';
+    }
+
+    webSocket.connect({}, function (frame: any) {
+        webSocket.subscribe("/sub/chat/room/" + params.roomId, function (message) {
+            const recv = JSON.parse(message.body);
+            // recvMessage(recv);
+        });
+        webSocket.send("/pub/chat/Message", {}, JSON.stringify({ chatroomId: params.roomId, regTime: '10', memberId: myUserId, isNotice: false, type: 'ENTER' }));
+    }, function (error: any) {
+        console.log(error)
+    })
+
+
 
     // 서버에서 불러온 해당 채팅방의 모든 채팅을 화면에 렌더링 해줄 함수
     const TextListRendering = () => {
@@ -237,6 +196,7 @@ export default function ChattingRoomPage() {
             )
             setInputValue("")
             setActivateSend("opacity-40")
+            sendMessage(inputValue)
             inputRef.current.focus()
         }
     }
@@ -256,6 +216,7 @@ export default function ChattingRoomPage() {
         dispatch(setTabName(`roomid=${params.roomId}에 해당하는 팀 이름 넣기`))
     }, [])
 
+    // 채팅이 올라올 때 마다 스크롤도 같이 움직임
     useEffect(() => {
         window.scrollTo(0, scrollRef.current.scrollHeight)
     }, [textList])
