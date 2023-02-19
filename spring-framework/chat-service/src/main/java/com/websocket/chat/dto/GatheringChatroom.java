32478de12a074a4700d96613a6dacb29d88f6c09
유도열 <oktui2002@naver.com>
2023-02-19 22:12:55 +0900

    fix unreadMessageNumber

--- spring-framework/chat-service/src/main/java/com/websocket/chat/dto/GatheringChatroom.java
+++ spring-framework/chat-service/src/main/java/com/websocket/chat/dto/GatheringChatroom.java
@@ -28,7 +28,7 @@ public class GatheringChatroom implements Serializable {
     private String lastMessageContent;
     @Transient
     @ApiModelProperty(value = "채팅방의 안 읽은 메세지 개수")
-    private String unreadMessageNumber;
+    private int unreadMessageNumber;
 
     @OneToOne
     @JoinColumn(name = "gatheringId", insertable=false, updatable = false)
