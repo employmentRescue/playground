8e0c039405d6b59334a88d96f4f2763594e5d9f3
skylove0911 <skylove0911@naver.com>
2023-02-19 18:35:43 +0900

    Update LoginSuccessPage.tsx

--- react-framework/src/pages/user/LoginSuccessPage.tsx
+++ react-framework/src/pages/user/LoginSuccessPage.tsx
@@ -24,7 +24,7 @@ export default function LoginSuccessPage() {
         appId: "1:808423483984:web:abdb73b3b73219b3b1bf55",
         measurementId: "G-S20W3SX3K1"
     };
-    const params = new URLSearchParams(location.search);
+    const params = new URLSearchParams(window.location.search);
     console.log("params: ", params);
     navigate("/home")
     // Initialize Firebase
