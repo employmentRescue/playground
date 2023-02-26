4eae0f026e170cc21fb874752c94b5cfc7ebd501
hoan123432 <hoan123432@naver.com>
2023-02-27 00:53:14 +0900

    login page app, web 구분용

--- react-framework/src/pages/user/LoginPage.tsx
+++ react-framework/src/pages/user/LoginPage.tsx
@@ -17,10 +17,10 @@ export default function LoginPage() {
             <img className="w-200 rounded-100 shadow-md" src={welcomeImg} />
             <h1 className="text-left font-bold text-35  mt-65 mb-25">환영합니다</h1>
             <h2 className="text-left font-bold text-20 text-gray-service to-transparent  opacity-50 mt-24 mb-40">당신을 위한 스포츠 팀 매칭 서비스</h2>
-            <a href={window.isFluttApp ? KAKAO_LOGIN_TEST_SERVER_URL + "/oauth2/app/login/kakao" : KAKAO_LOGIN_TEST_SERVER_URL + "/oauth2/web/login/kakao"} onClick={movePage}>
+            <div onClick={movePage}>
                 <img src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg"
                     width="200" />
-            </a>
+            </div>
         </div>
     )
 }
