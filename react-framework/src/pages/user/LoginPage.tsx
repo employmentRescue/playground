f4e10b862501b4cd1b2915bc09ec9d8fdfe41905
hoan123432 <hoan123432@naver.com>
2023-02-27 00:45:12 +0900

    login page app, web 구분용

--- react-framework/src/pages/user/LoginPage.tsx
+++ react-framework/src/pages/user/LoginPage.tsx
@@ -7,7 +7,8 @@ import { useState } from "react";
 
 export default function LoginPage() {
     const [LoginEnable, setLoginEnable] = useState(false);
-    function movePage(s: String) {
+    function movePage(e) {
+        e.preventDefault()
         console.log("LoginPage(movePage)", window.isFluttApp)
     }
 
@@ -16,7 +17,7 @@ export default function LoginPage() {
             <img className="w-200 rounded-100 shadow-md" src={welcomeImg} />
             <h1 className="text-left font-bold text-35  mt-65 mb-25">환영합니다</h1>
             <h2 className="text-left font-bold text-20 text-gray-service to-transparent  opacity-50 mt-24 mb-40">당신을 위한 스포츠 팀 매칭 서비스</h2>
-            <a href={window.isFluttApp ? KAKAO_LOGIN_TEST_SERVER_URL + "/oauth2/app/login/kakao" : KAKAO_LOGIN_TEST_SERVER_URL + "/oauth2/web/login/kakao"} onclick={() => movePage("move")}>
+            <a href={window.isFluttApp ? KAKAO_LOGIN_TEST_SERVER_URL + "/oauth2/app/login/kakao" : KAKAO_LOGIN_TEST_SERVER_URL + "/oauth2/web/login/kakao"} onClick={movePage}>
                 <img src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg"
                     width="200" />
             </a>
