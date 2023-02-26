b9fe6e203893c9c291c89c4d024776de50467b95
hoan123432 <hoan123432@naver.com>
2023-02-26 19:11:47 +0900

    service registry

---
+++ spring-framework/service-registry/src/test/java/com/ssafy/edu/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.edu.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
