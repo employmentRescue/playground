a73ca11d75a906d653de7637f2d573b097c74258
hoan123432 <hoan123432@naver.com>
2023-02-26 21:30:51 +0900

    service discovery

--- spring-framework/api-gateway/src/main/java/com/ssafy/edu/api_gateway/ApiGatewayApplication.java
+++ spring-framework/api-gateway/src/main/java/com/ssafy/edu/api_gateway/ApiGatewayApplication.java
@@ -2,8 +2,9 @@ package com.ssafy.edu.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
