b758f4a9185ee6efe6c842194255cbd12417ef1e
hoan123432 <hoan123432@naver.com>
2023-02-26 20:04:47 +0900

    service discovery

--- docker-data-server/mysql-init-files/create_table.sql
+++ docker-data-server/mysql-init-files/create_table.sql
@@ -12,6 +12,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- Schema playground
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `playground` DEFAULT CHARACTER SET utf8 ;
+ALTER AUTHORIZATION ON DATABASE::MyDatabase TO ssafy;
 USE `playground` ;
 
 -- -----------------------------------------------------
