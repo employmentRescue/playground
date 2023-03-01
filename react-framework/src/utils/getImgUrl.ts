2085cb8171e4a1840c661771830e1a68b2174e7f
skylove0911 <skylove0911@naver.com>
2023-03-01 17:16:20 +0900

    update url

--- react-framework/src/utils/getImgUrl.ts
+++ react-framework/src/utils/getImgUrl.ts
@@ -1,5 +1,5 @@
 //https://i8b309.p.ssafy.io/src/assets/
 //https://localhost:3000/src/assets/
 export function getImgUrl(url: string, name: string | number) {
-  return new URL(url + '/' + name + '.png', "https://i8b309.p.ssafy.io/assets/").href
+  return new URL(url + '/' + name + '.png', "https://wwww.b309-playground.com/assets/").href
 }
